ca8f4abae527f6fe64e0289db7ff9938309b044f
xusanboyshavkatov <xusanboyshavkatov0110@gmail.com>
2024-08-13 10:14:21 +0500

    Update 13.08.2024
    Update 13.08.2024

--- src/page/footer/Footer.jsx
+++ src/page/footer/Footer.jsx
@@ -8,7 +8,37 @@ const Footer = () => {
             <div className="footer_content">
                 <div className="footer_content_logo_inf">
                     <img src=".././src/img/logo.png" alt="" />
+                    <h1>Sifat, dizayn va qulaylikning
+                        ideal uyg'unligi</h1>
                 </div>
+
+                <div className="footer_content_contact_inf">
+                    <h1>Kontakt:</h1>
+                    <ul>
+                        <li>
+                            <i className="fa-solid fa-phone" />
+                            <a href="">
+                                Telefon<br />
+                                +998 88 142 72-71
+                            </a>
+                        </li>
+                        <li>
+                            <i className="fa-solid fa-location-dot" />
+                            <a href="">
+                                Bizning manzil<br />
+                                Namangan Shahar, Orzu MFY Ijodkor 110
+                            </a>
+                        </li>
+                        <li>
+                            <i className="fa-solid fa-envelope"/>
+                            <a href="">
+                                Elektron pcohta<br />
+                                +998 88 142 72-71
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+
                 <div className="footer_content_menu_link">
                     <h1>Sahifalar</h1>
                     <ul>
@@ -17,15 +47,6 @@ const Footer = () => {
                         <li><Link to="haqimizda" className='Link' smooth={true} duration={50}>Haqimizda</Link></li>
                         <li><Link to="portfolio" className='Link' smooth={true} duration={50}>Portfolio</Link></li>
                         <li><Link to="kontakt" className='Link' smooth={true} duration={50}>Kontakt</Link></li>
-                    </ul>
-                </div>
-                <div className="footer_content_contact_inf">
-                    <h1>Boshqa</h1>
-                    <ul>
-                        <li><a href="">+998 88 142 72-71</a></li>
-                        <li><a href="">Andijon viloyat, Baliqchi tushman, <br />Chinobod shaxar</a></li>
-                        <li><a href="">Ish vaqti: 10:00 dan <br /> 00:00gacha</a></li>
-
                     </ul>
                 </div>
             </div>
